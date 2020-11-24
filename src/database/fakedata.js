137a36f540e73aaffa8644ea54c4470330e0176c
Jonathan Tassoni <info.jonathantassoni@gmail.com>
2020-11-23 22:04:16 -0300

    add project

---
+++ src/database/fakedata.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: 1,
+    lat: -27.2057828,
+    ing:  -49.639837 ,
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a criaças de 06 a 15 anos que se encontre em suação de risco e/ou vulnerabilidade social.",
+    images: [
+          "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+          "https://images.unsplash.com/photo-1573924436910-447d0d2176c9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+          
+      ],
+      instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+      opening_hours: "Horário de visitas Das 18 até 8h",
+      open_on_weekends: "1"
+  },
+
+
+
+
+
+
+
+  {
+    id: 2,
+    lat: -27.2057828,
+    ing:  -49.639837 ,
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a criaças de 06 a 15 anos que se encontre em suação de risco e/ou vulnerabilidade social.",
+    images: [
+          "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+          "https://images.unsplash.com/photo-1573924436910-447d0d2176c9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+          
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+    opening_hours: "Horário de visitas Das 18 até 8h",
+    open_on_weekends: "0"
+  },
+];
